e7a4da20eb57258c309e58f9bd45821f2e3aac04
Tobias Källkvist <tobias.kallkvist@iths.se>
2022-01-28 16:10:14 +0100

    Removed navigation script and added background elements

--- script/script.js
+++
@@ -1,9 +0,0 @@
-onload = function() {
-    navigate('pages/about.html');
-}
-
-function navigate(path) {
-    fetch(path)
-    .then(data => data.text())
-    .then(html => document.getElementById('contentArea').innerHTML = html);
-}
